537777a69b7c19a0900a0185a88599ea5991f586
Rownaknira <rownaknira@yahoo.com>
2016-10-26 12:38:44 +0600

    Retrieve, Update and Delete product async

--- app/daos/ProductDao.java
+++ app/daos/ProductDao.java
@@ -12,21 +12,43 @@ import javax.persistence.Persistence;
  * Created by rownak on 10/25/16.
  */
 public class ProductDao {
-    static EntityManagerFactory emf;
+    static EntityManagerFactory entityManagerFactory;
 
     public JsonNode create(Product product){
-        EntityManager em = getEmf().createEntityManager();
-        em.persist(product);
-        em.close();
+        EntityManager entityManager = getEmf().createEntityManager();
+        entityManager.persist(product);
+        entityManager.close();
 
         return Json.toJson(product);
     }
 
+    public Product getProductById(String productId) {
+        EntityManager entityManager = getEmf().createEntityManager();
+        Product product = entityManager.find(Product.class, productId);
+        entityManager.close();
+
+        return product;
+    }
+
+    public JsonNode update(Product product) {
+        EntityManager entityManager = getEmf().createEntityManager();
+        Product updatedProduct = entityManager.merge(product);
+        entityManager.close();
+
+        return Json.toJson(updatedProduct);
+    }
+
+    public void delete(Product product) {
+        EntityManager entityManager = getEmf().createEntityManager();
+        entityManager.remove(product);
+        entityManager.close();
+    }
+
     private static EntityManagerFactory getEmf() {
-        if (emf == null) {
-            emf = Persistence.createEntityManagerFactory("cassandra_pu");
+        if (entityManagerFactory == null) {
+            entityManagerFactory = Persistence.createEntityManagerFactory("cassandra_pu");
         }
 
-        return emf;
+        return entityManagerFactory;
     }
 }
--- app/services/ProductService.java
+++ app/services/ProductService.java
@@ -1,6 +1,7 @@
 package services;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.inject.Singleton;
 import daos.ProductDao;
 import models.Product;
 import play.libs.Json;
@@ -11,6 +12,7 @@ import java.util.UUID;
  * Created by rownak on 10/25/16.
  */
 
+@Singleton
 public class ProductService {
     ProductDao productDao = new ProductDao();
 
@@ -18,5 +20,30 @@ public class ProductService {
         Product product = Json.fromJson(jsonNode, Product.class);
         product.setProductId(UUID.randomUUID().toString());
         return productDao.create(product);
+    }
+
+    public JsonNode update(JsonNode jsonNode, String productId) {
+        Product product = productDao.getProductById(productId);
+        Product formProduct = Json.fromJson(jsonNode, Product.class);
+
+        if (formProduct.getProductName().trim() != null) {
+            product.setProductName(formProduct.getProductName());
+        }
+
+        if (formProduct.getPrice().toString().trim() != null) {
+            product.setPrice(formProduct.getPrice());
+        }
+
+        return productDao.update(product);
+    }
+
+    public void delete(String productId) {
+        Product product = productDao.getProductById(productId);
+        productDao.delete(product);
+    }
+
+    public JsonNode get(String productId) {
+        Product product = productDao.getProductById(productId);
+        return Json.toJson(product);
     }
 }
